71e8dbf1dcbde65c25980c56eef5bf223d6cc87b
Pablo De Nápoli <pdenapo@gmail.com>
2025-02-26 15:40:34 -0300

    We added a button and a counter, showing basic reactivity.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,26 @@
+<script>
+	let count = 0;
+
+	import {
+		Button,
+	} from 'svelte-ux';
+
+	
+	function handleClick() {
+		count += 1;
+	}
+	
+</script>
+
 <main class="p-4 grid place-items-center content-center">
 	<h1 class="text-2xl font-semibold m-4 text-secondary">
 		Hello World, Svelte-ux!
 	</h1>
+
+    <Button variant="fill" color="primary" on:click={handleClick}> Click me!</Button>
+
+	<div class="p-3">
+	Clicks= {count}
+	</div>
 
 </main>
